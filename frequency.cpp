bdf99bda21fc1108c86423e08c53eab352c8221f
mohan-evin7 <57702584+mohan-evin7@users.noreply.github.com>
2020-08-06 14:09:11 +0530

    Create frequency.cpp

---
+++ frequency.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+       cin>>a[i]; 
+    }
+    int x;
+    cin>>x;
+    int c=0;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==x)
+        c++;
+    }
+    cout<<c;
+    return 0;
+}
